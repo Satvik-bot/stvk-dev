0a91f8f3ccf264db75b86e509de5ada39d61ae5f
Satvik Sharma <satvik-bot>
2025-02-24 20:11:43 +0530

    analytics-vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { useState } from "react";
 import "./App.css";
 import About from "./components/about";
@@ -8,6 +9,7 @@ function App() {
      <About></About>
       <div className="projects">
         <h2>Projects : WIP ⚒️</h2>
+        <Analytics/>
       </div>
     </div>
   );
